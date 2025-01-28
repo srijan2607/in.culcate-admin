32af9ee6d2a6014b45386d66fed05209b57a44a5
srijan <srijan.mart@gmail.com>
2025-01-28 18:24:37 +0530

    Add

--- backend/controllers/Article/Article.js
+++
@@ -1,7 +0,0 @@
-// Inside Routes/Article.js (or wherever your route handler is)
-const sendArticle = (req, res) => {
-  // Your logic for handling the request
-  res.status(200).json({ message: "Article sent" });
-};
-
-module.exports = sendArticle;
--- backend/routers/register.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const userRegister = require("../controllers/Auth/user/register")
-
-
-router.post("/", userRegister);
-
-module.exports = router;
